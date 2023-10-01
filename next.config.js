e3bad3a3f1abb54a02eef67e2b147192c572f53f
saraanasim <saraanofficial@gmail.com>
2023-10-02 02:47:40 +0500

    Created build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  // Add basePath
+  basePath: '/github-pages',
   trailingSlash: true,
   pageExtensions: ['page.js', 'api.js'],
   webpack(config, { isServer }) {
